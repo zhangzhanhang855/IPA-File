ebda4c08f5ed43c25420bf133fff1f3e99d64ef6
zhangzhanhang855 <zhangzhanhang855@gmail.com>
2026-08-30 16:51:09 +0800

    Update URL in viewDidLoad method

--- StockScope/ViewController.swift
+++ StockScope/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let u = URL(string: "https://musicplayer-ruby-delta.vercel.app") {
+        if let u = URL(string: "https://jr-staff-center.onrender.com/admin.html") {
             webView.load(URLRequest(url: u))
         }
     }
